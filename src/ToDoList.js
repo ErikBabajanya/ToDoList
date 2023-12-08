eeec874a30622a19c6a4c4909463f1e05ed61203
ErikBabajanyan <Erik.Babajanyan10@gmail.com>
2023-12-08 18:46:13 +0400

    nor avelacum

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,26 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ToDoList({ toDoList, onChange, onDelete }) {
+  const [editingIndex, setEditingIndex] = useState(null);
+  const [editedText, setEditedText] = useState('');
+
+  const handleEditStart = (index, text) => {
+    setEditingIndex(index);
+    setEditedText(text);
+  };
+
+  const handleEditCancel = () => {
+    setEditingIndex(null);
+    setEditedText('');
+  };
+
+  const handleEditSave = (index) => {
+    onChange(index, {
+      ...toDoList[index],
+      text: editedText,
+    });
+    setEditingIndex(null);
+    setEditedText('');
+  };
+
   return (
     <div className='List'>
-      {
-        toDoList.map((list, index) => (
-          <div key={index}>
-            <input
-              type="checkbox"
-              onChange={(e) => {
-                onChange(index, {
-                  ...list,
-                  completed: e.target.checked,
-                });
-              }}
-              checked={list.completed}
-            />
-            <span>{list.text}</span>
-            <button onClick={() => onDelete(list.id)}>X</button>
-          </div>
-        ))
-      }
+      {toDoList.map((list, index) => (
+        <div key={index}>
+          <input
+            type="checkbox"
+            onChange={(e) => {
+              onChange(index, {
+                ...list,
+                completed: e.target.checked,
+              });
+            }}
+            checked={list.completed}
+          />
+          {editingIndex === index ? (
+            <div>
+              <input
+                className='Change-task'
+                type="text"
+                value={editedText}
+                onChange={(e) => setEditedText(e.target.value)}
+              />
+              <button className='change' onClick={() => handleEditSave(index)}>Save</button>
+              <button className='change' onClick={handleEditCancel}>Cancel</button>
+            </div>
+          ) : (
+            <div>
+              <span onClick={() => handleEditStart(index, list.text)}>{list.text}</span>
+              <span className={list.completed ? "online" : "offline"} />
+              <button onClick={() => onDelete(list.id)}>X</button>
+            </div>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
